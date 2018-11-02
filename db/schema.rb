1e3d7d44e86641960c1931a2c2a1fc9828c7c304
Chloe Baron <chloe.f.baron@gmail.com>
2018-11-01 21:06:40 -0300

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_01_203109) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
